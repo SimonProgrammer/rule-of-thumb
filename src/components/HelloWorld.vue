1e932b435fedfbf62a3bfbd42a97564d088fcb56
Simon Alberto Lopez Higuera <lopezhsimon@gmail.com>
2022-06-14 01:38:19 -0500

    basic configuration project finished

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts">
+import { defineComponent } from 'vue'
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+export default defineComponent({
+  props:{
+    msg: { type: String, required: true },
+  },
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    add(){
+      this.count++;
+    }
+  }
+})
 </script>
 
 <template>
@@ -26,7 +36,7 @@ const count = ref(0)
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
+  <button type="button" @click="add">count is: {{ count }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import './assets/css/variables.css'
 
-createApp(App).mount('#app')
+import en from './locales/en.json'
+
+const i18n = createI18n({
+    legacy: false,
+    locale: 'en',
+    messages: {
+      en
+    }
+})
+
+createApp(App).use(i18n).mount('#app')
